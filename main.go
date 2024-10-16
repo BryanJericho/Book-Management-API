7fcc8e2fcb42ab547528073b4b4ebb31de90449d
BryanJericho <bryanpanggalo@gmail.com>
2024-10-16 20:32:01 +0800

    initial commit

---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var books []Book
+var idCounter int
+
+// Get all books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+// Get a single book
+func getBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid book ID", http.StatusBadRequest)
+		return
+	}
+
+	for _, book := range books {
+		if book.ID == id {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+
+	http.Error(w, "Book not found", http.StatusNotFound)
+}
+
+// Create a new book
+func createBook(w http.ResponseWriter, r *http.Request) {
+	var newBook Book
+	json.NewDecoder(r.Body).Decode(&newBook)
+	idCounter++
+	newBook.ID = idCounter
+	books = append(books, newBook)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(newBook)
+}
+
+// Update an existing book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid book ID", http.StatusBadRequest)
+		return
+	}
+
+	for i, book := range books {
+		if book.ID == id {
+			json.NewDecoder(r.Body).Decode(&book)
+			books[i] = book
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+
+	http.Error(w, "Book not found", http.StatusNotFound)
+}
+
+// Delete a book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid book ID", http.StatusBadRequest)
+		return
+	}
+
+	for i, book := range books {
+		if book.ID == id {
+			books = append(books[:i], books[i+1:]...)
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+	}
+
+	http.Error(w, "Book not found", http.StatusNotFound)
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	// Dummy data
+	books = append(books, Book{ID: 1, Title: "Golang Simplified", Author: "John Doe"})
+
+	// Routes
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/books", createBook).Methods("POST")
+	router.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
+
+	fmt.Println("Server is running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
